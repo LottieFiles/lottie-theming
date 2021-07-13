089c7e3f8c617b8dde7cec900afbb6dc1e8c64f7
karamalie <karam7898@gmail.com>
2021-07-13 16:45:44 +0800

    load lottie, dev libraries, rollup fixed

--- rollup.config.js
+++ rollup.config.js
@@ -1,120 +1,58 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import strip from '@rollup/plugin-strip';
-import dts from 'rollup-plugin-dts';
-import esbuild from 'rollup-plugin-esbuild';
-import filesize from 'rollup-plugin-filesize';
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import copy from "rollup-plugin-copy";
+import filesize from "rollup-plugin-filesize";
+import resolve from "@rollup/plugin-node-resolve";
+import serve from "rollup-plugin-serve";
+import { terser } from "rollup-plugin-terser";
+import typescript2 from "rollup-plugin-typescript2";
 
-import pkg from './package.json';
+const production = !process.env.ROLLUP_WATCH;
+const extensions = [".js", ".jsx", ".ts", ".tsx", ".mjs"];
+const outputDir = "./dist/";
 
-// Whether current node environment is production mode
-const isProduction = process.env.NODE_ENV === 'production';
+export default {
+  input: "./src/index.ts",
+  treeshake: false,
+  output: [
+    {
+      file: "./dist/lottie-theming.esm.js",
+      // dir: outputDir,
+      format: "es",
+      sourcemap: true,
+    },
+    {
+      file: "./dist/lottie-theming.js",
+      format: "umd",
+      name: "lottie-theming",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    resolve({ extensions }),
+    commonjs({ include: /node_modules/ }),
+    typescript2({
+      check: false,
+    }),
+    babel({
+      extensions: extensions,
+      exclude: ["./node_modules/@babel/**/*", "./node_modules/core-js/**/*"],
+    }),
+    !production &&
+      copy({
+        targets: [
+          { src: "./src/index.html", dest: outputDir }
+        ],
+      }),
+    filesize(),
+    !production &&
+      serve({
+        contentBase: [outputDir],
+        open: true,
+        host: "localhost",
+        port: 10000,
+      }),
 
-// Banner
-const compiledAt = new Date().toUTCString().replace(/GMT/g, 'UTC');
-const banner = [
-  `/*!`,
-  ` * ${pkg.name} - v${pkg.version}`,
-  ` * Compiled ${compiledAt}`,
-  ` *`,
-  ` * Copyright LottieFiles. All rights reserved.`,
-  ` */`,
-].join('\n');
-
-// String replacement definitions
-const replacements = {
-  'process.env.GENERATOR': `'${pkg.name} ${pkg.version}'`,
+    production && terser(),
+  ],
 };
-
-export default [
-  // UMD build for browser
-  {
-    input: 'src/index.ts',
-    output: [
-      {
-        banner,
-        file: pkg.browser,
-        format: 'umd',
-        name: 'Lottie',
-        exports: 'named',
-        sourcemap: true,
-      },
-    ],
-
-    plugins: [
-      // Resolve node_modules
-      resolve({
-        browser: true,
-      }),
-
-      // Convert CJS modules to ES
-      commonjs(),
-
-      // Remove debugger statements and console.log calls
-      isProduction && strip(),
-
-      // Build
-      esbuild({
-        minify: isProduction,
-        target: 'es2015', // default, or 'es20XX', 'esnext'
-
-        // String replacements
-        define: replacements,
-      }),
-
-      filesize(),
-    ],
-  },
-
-  // CJS build for NodeJS and ES build for bundlers and modern browsers
-  {
-    input: 'src/index.ts',
-
-    output: [
-      {
-        banner,
-        file: pkg.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        banner,
-        file: pkg.module,
-        format: 'es',
-        sourcemap: true,
-      },
-    ],
-
-    external: Object.keys(pkg.dependencies || {}),
-
-    plugins: [
-      // Remove debugger statements and console.log calls
-      isProduction && strip(),
-
-      // Build
-      esbuild({
-        // minify: isProduction,
-        target: 'es2018', // default, or 'es20XX', 'esnext'
-
-        // String replacements
-        define: replacements,
-      }),
-
-      filesize(),
-    ],
-  },
-
-  // Build Typescript type declarations
-  {
-    input: 'src/index.ts',
-
-    output: [
-      {
-        file: 'dist/index.d.ts',
-        format: 'es',
-      },
-    ],
-
-    plugins: [dts()],
-  },
-];
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,84 @@
 export class LottieTheming {
   constructor() {}
+
+  /**
+   * Check to see whether parameter passed in is json url or json object
+   */
+  parseSrc(src: string | object): string | object {
+    if (typeof src === 'object') {
+      return src;
+    }
+
+    try {
+      return JSON.parse(src);
+    } catch (e) {
+      // Try construct an absolute URL from the src URL
+      const srcUrl: URL = new URL(src, window.location.href);
+
+      return srcUrl.toString();
+    }
+  }
+  /**
+   * Validate lottie
+   */
+  isLottie(json: Record<string, any>): boolean {
+    const mandatory = ['v', 'ip', 'op', 'layers', 'fr', 'w', 'h'];
+
+    return mandatory.every(field => Object.prototype.hasOwnProperty.call(json, field));
+  }
+  /**
+   * if url is provided. get the json object
+   */
+  async fromURL(url: string): Promise<Record<string, any>> {
+    if (typeof url !== 'string') {
+      throw new Error(`The url value must be a string`);
+    }
+
+    let json;
+
+    try {
+      // Try construct an absolute URL from the src URL
+      const srcUrl: URL = new URL(url);
+
+      // Fetch the JSON file from the URL
+      const result = await fetch(srcUrl.toString());
+
+      json = await result.json();
+    } catch (err) {
+      throw new Error(`An error occurred while trying to load the Lottie file from URL`);
+    }
+
+    return json;
+  }
+  /**
+   * initialise
+   */
+  async init(src: string | object) {
+    let srcParsed = this.parseSrc(src);
+    let jsonData = {};
+
+    let srcAttrib = typeof srcParsed === 'string' ? 'path' : 'animationData';
+    // Fetch resource if src is a remote URL
+    if (srcAttrib === 'path') {
+      jsonData = await this.fromURL(srcParsed as string);
+      srcAttrib = 'animationData';
+    } else {
+      jsonData = srcParsed;
+    }
+    if (!this.isLottie(jsonData)) {
+      // throw error
+      // this.dispatchEvent(new CustomEvent(PlayerEvents.Error));
+    }
+    return jsonData;
+  }
+
+  public tokenize() {}
+  // style tokenizer
+  // function to extract style tokens from a lottie object
+  // json based tree exploration to find all stylable elements
+  // start with color first
+  // json paths as primary identifier and alias as nm
+
+  // style applicator
+  // functioon to apply style token and config to a lottie object
 }
