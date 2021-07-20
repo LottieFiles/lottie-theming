aff0d4521dd333911a7297357cc127c1a747c8ab
Alex Harper <alex@lottiefiles.com>
2021-07-20 16:01:39 +0100

    chore: modified demo to test theme loader

--- src/demo.js
+++ src/demo.js
@@ -1,9 +1,29 @@
-var LottieTheming = require('./lottie-theming.js');
+/**
+ * Copyright 2021 Design Barn Inc.
+ */
+
+const { LottieTheming, ThemeConfig } = require('./lottie-theming.js');
+
+const fs = require('fs');
+const console = require('console');
 
 async function startThemer() {
   let themer = new LottieTheming();
   let data = await themer.init('https://assets3.lottiefiles.com/packages/lf20_wgh8xmh0.json');
+
   themer.tokenize();
 }
 
-startThemer();
+async function loadConfig() {
+  const json = fs.readFileSync('./demo-config.json');
+
+  //console.log(`parsing config.. ${json}`);
+
+  const themeConfig = ThemeConfig.fromJSON(json);
+
+  console.log(themeConfig.themes);
+}
+
+//startThemer();
+
+loadConfig();
